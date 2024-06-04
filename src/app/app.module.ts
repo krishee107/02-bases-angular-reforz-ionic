66f7b298c1304f552ed1f31d237a89bf87c6f918
krishee107 <krishee107@gmail.com>
2024-06-04 23:17:22 +0200

    commit inicial + componentes iniciales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
