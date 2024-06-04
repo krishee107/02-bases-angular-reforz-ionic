0f61b8d839cd5dcbc828253b0076661135c5b5c9
krishee107 <krishee107@gmail.com>
2024-06-04 23:30:24 +0200

    Menu con routerLink y ngfor

---
+++ src/app/components/menu/menu.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.css'
+})
+export class MenuComponent {
+  rutas = [
+    { path: 'home', name: 'Home' },
+    { path: 'about', name: 'About' },
+    { path: 'contact', name: 'Contact' },
+  ];
+
+}
